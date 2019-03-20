5a63965c9b4a95e023a988f1f3cb1b78a7c9dd02
dnlklbv <hi.daniil.k@gmail.com>
2019-03-20 21:57:09 +0300

    feat: implement getZerosCount function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,49 @@
 module.exports = function getZerosCount(number, base) {
-  // your implementation
-}
+  function factorizer(x) {
+    const arr = [];
+    if (x > 0 && x !== 1) {
+      for (let i = 2; i <= x; i += 1) {
+        while (x % i === 0 && x > 1) {
+          arr.push(i);
+          x /= i;
+        }
+        if (x / i === 1) {
+          arr.push(x);
+        }
+      }
+      return arr;
+    } if (x === 1) {
+      return 1;
+    }
+    return NaN;
+  }
+
+  function sum(n, p) {
+    const k = Math.floor(Math.log(n) / Math.log(p));
+    let res = 0;
+    let divider = p;
+    for (let i = 1; i <= k; i += 1) {
+      res += Math.floor(n / divider);
+      divider *= p;
+    }
+    return res;
+  }
+
+  const primeNums = factorizer(base);
+  const uniquePrimeNums = [];
+  const uniqueRepeatings = {};
+
+  for (let i = 0; i < primeNums.length; i += 1) {
+    if (!uniquePrimeNums.includes(primeNums[i])) {
+      uniquePrimeNums.push(primeNums[i]);
+      uniqueRepeatings[primeNums[i]] = 1;
+    } else { uniqueRepeatings[primeNums[i]] += 1; }
+  }
+
+  let min = Infinity;
+  for (let i = 0; i < uniquePrimeNums.length; i += 1) {
+    min = Math.min(min, sum(number, uniquePrimeNums[i]) / uniqueRepeatings[uniquePrimeNums[i]]);
+  }
+
+  return Math.floor(min);
+};
